edf2bab7bf80c46d364185c981d665023c55a9c4
TomateFarcie <leclerc.charlene@gmail.com>
2023-09-23 10:51:16 +0200

    change import by use

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../images/Rework_LOGO_Trinitite_Black_BG.png';
+//import logo from '../images/Rework_LOGO_Trinitite_Black_BG.png';
 import Members from './Members'
 
 const About = () => {
@@ -7,7 +7,7 @@ const About = () => {
         <section className="about" id='about'>
             <div className='about-container'>
                 <div className="box">
-                    <img src={logo} alt="" />
+                    {/*<img src={logo} alt="" />*/}
                     <div className="resume">
                         <h2>A PROPOS DE NOUS</h2>
                         <h3>DEVELOPPEUR ET GRAPHISTES/DESGINER</h3>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import baniere from '../images/Baniiere_Artstation.jpg'
+//import baniere from '../images/Baniiere_Artstation.jpg'
 
 const Home = () => {
     return (
         <section className='home' id='home'>
             <div className='studio-container'>
-                <img src={baniere} alt="" />
+                {/*<img src={baniere} alt="" />*/}
                 <div className="box">
                     <h1>TRINITITE STUDIO</h1>
                     <h3>Studio indépendant de développement de jeux vidéo</h3>
