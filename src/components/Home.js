944d99358e71756eac1b447d4f05da430d300ee4
TomateFarcie <leclerc.charlene@gmail.com>
2023-09-23 11:12:37 +0200

    test

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <section className='home' id='home'>
             <div className='studio-container'>
-                {/*<img src={baniere} alt="" />*/}
+                <img src={process.env.PUBLIC_URL + "/images/Baniiere_Artstation.jpg"} alt="" />
                 <div className="box">
                     <h1>TRINITITE STUDIO</h1>
                     <h3>Studio indépendant de développement de jeux vidéo</h3>
